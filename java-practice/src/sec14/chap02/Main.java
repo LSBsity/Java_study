499aaae82ceb71b1f75a57a2df9c1c538c5de760
LSBsity <dltmdqls51@naver.coml>
2023-09-24 00:18:17 +0900

    JAR

---
+++ java-practice/src/sec14/chap02/Main.java
@@ -0,0 +1,11 @@
+package sec14.chap02;
+
+import com.petshop.Dog;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+                new Dog("멍돌이").makeSound()
+        );
+    }
+}
